dc7bc8a3f0097d80997cb576ebb229ca8f3fdf02
Davide Bosco <info.modestogames@gmail.com>
2024-11-21 17:38:03 +0100

    Fix UI errors

--- Assets/Scripts/UI/NotificationItemsHandler.cs
+++ Assets/Scripts/UI/NotificationItemsHandler.cs
@@ -1,20 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NotificationItemsHandler : MonoBehaviour
 {
     [SerializeField] private NotificationItem _itemPrefab;
     [SerializeField] private Transform _itemContainer;
+    [SerializeField] private RectTransform _layoutGroup;
 
     private List<NotificationItem> _items = new List<NotificationItem>();
 
     public void AddItem(int id, AndroidJavaObject androidNotificationDto)
     {
-        Debug.Log("###ADD ITEM");
         NotificationDTO dto = new NotificationDTO(id, androidNotificationDto);
         Debug.Log(dto);
         var item = Instantiate(_itemPrefab, _itemContainer);
         item.Initialize(dto);
         _items.Add(item);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_layoutGroup);
     }
 }
